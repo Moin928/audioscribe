accb204cf6422e8ecb9e3de8b338634953c1537d
Moin928 <moinqureshi928@gmail.com>
2026-08-11 00:13:09 +0530

    Add initial test class for Audiosciber application

---
+++ src/test/java/com/moin/audiosciber/AudiosciberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moin.audiosciber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudiosciberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
